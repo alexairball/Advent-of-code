eb4effc7242b30d9ef578d07172ef0aeea2055c4
alexairball <alexgoodsun@hotmail.com>
2021-12-02 19:42:37 -0500

    Day 3 Ready

---
+++ code/2021/03.js
@@ -0,0 +1,5 @@
+import _ from "lodash";
+
+export const code_2021_03 = (fileContent) => {
+  console.log('hello');
+};
--- import.js
+++ import.js
@@ -1,7 +1,9 @@
 import { code_2021_01 } from './code/2021/01.js';
 import { code_2021_02 } from './code/2021/02.js';
+import { code_2021_03 } from './code/2021/03.js';
 
 export const all =  {
     code_2021_01,
-    code_2021_02
+    code_2021_02,
+    code_2021_03
 };
